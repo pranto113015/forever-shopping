99f2bef7f6313db910b6a78428e5ffa1bd10398e
pranto113015 <pranto3876@gmail.com>
2024-09-28 22:32:33 +0600

    add bestseller title and subtitle

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
 
 const BestSeller = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { products } = useContext(ShopContext);
+  const [bestSeller, setBestSeller] = useState([]);
 
-export default BestSeller
+  useEffect(() => {
+    const bestProduct = products.filter((item) => {
+      item.bestSeller;
+    });
+    setBestSeller(bestProduct.slice(0, 5));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className="text-center text-3xl py-8">
+        <Title text1={"BEST"} text2={"SELLERS"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, soluta
+          odit et nisi amet maiores.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BestSeller;
