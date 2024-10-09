524e4fe2de9d97ad1b93590c00e3f25da22efff9
pranto113015 <pranto3876@gmail.com>
2024-10-10 00:10:02 +0600

    done the left side element of  collection component

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react";
 import { ShopContext } from "./../context/ShopContext";
+import { assets } from "../assets/assets";
 
 const Collection = () => {
   const { products } = useContext(ShopContext);
@@ -9,8 +10,9 @@ const Collection = () => {
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
       {/* Filter options */}
       <div className="min-w-60">
-        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
+        <p onClick={()=>setShowFilter(!showFilter)} className="my-2 text-xl flex items-center cursor-pointer gap-2">
           FILTERS
+          <img className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} src={assets.dropdown_icon} alt="dropdown_icon" />
         </p>
 
         {/* Catagory Filter */}
@@ -41,11 +43,11 @@ const Collection = () => {
 
         {/* SubCategory Filter */}
         <div
-          className={`border border-gray-300 pl-5 py-3 mt-6 ${
+          className={`border border-gray-300 pl-5 py-3 my-5 ${
             showFilter ? "" : "hidden"
           } sm:block`}
         >
-          <p className="mb-3 text-sm font-medium">Type</p>
+          <p className="mb-3 text-sm font-medium">TYPE</p>
 
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <p className="flex gap-2">
@@ -65,6 +67,12 @@ const Collection = () => {
           </div>
         </div>
       </div>
+
+
+
+
+
+   
     </div>
   );
 };
