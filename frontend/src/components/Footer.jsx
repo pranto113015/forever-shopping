1712530637f32de74392251694b955d2ffa9f1e4
pranto113015 <pranto3876@gmail.com>
2024-10-13 19:24:20 +0600

    modify the footer component bottom title

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
 
       <div>
         <hr />
-        <p className="py-5 text-sm text-center">Copyright 2024 @ forever.com - All Right Reserved</p>
+        <p className="py-5 text-sm text-center">Copyright 2024 @ forever.com - All Right Reserved Design by Pranto Kumar</p>
       </div>
     </div>
   );
