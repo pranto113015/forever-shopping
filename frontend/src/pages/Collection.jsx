a51e809a806512ea1be73ada69445d7eec39bf02
pranto113015 <pranto3876@gmail.com>
2024-10-13 18:39:15 +0600

    add product in collection component

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,11 +1,17 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "./../context/ShopContext";
 import { assets } from "../assets/assets";
 import Title from "./../components/Title";
+import ProducItem from "./../components/ProducItem";
 
 const Collection = () => {
   const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
+  const [filterProducts, setFilterProducts] = useState([]);
+
+  useEffect(() => {
+    setFilterProducts(products);
+  }, []);
 
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
@@ -87,8 +93,20 @@ const Collection = () => {
             <option value="high-low">sort by: High to Low</option>
           </select>
         </div>
+
+        {/* Map product */}
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6">
+          {filterProducts.map((item, index) => (
+            <ProducItem
+              key={index}
+              name={item.name}
+              id={item._id}
+              price={item.price}
+              image={item.image}
+            />
+          ))}
+        </div>
       </div>
-      
     </div>
   );
 };
