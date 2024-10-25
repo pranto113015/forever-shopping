0170cb79f0b81fd9505bb0d4abbf803d1cc09f05
pranto113015 <pranto3876@gmail.com>
2024-10-25 20:01:35 +0600

    add to cart button in product component

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -75,6 +75,8 @@ const Product = () => {
               ))}
             </div>
           </div>
+
+          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">ADD TO CART</button>
         </div>
       </div>
     </div>
