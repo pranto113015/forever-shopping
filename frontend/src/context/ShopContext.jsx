f1b2c1d6fc593d35097abf9c44a9e41d74340f7e
pranto113015 <pranto3876@gmail.com>
2025-04-11 12:07:31 +0600

    add select product size and add to cart click event

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import PropTypes from 'prop-types';
 
 export const ShopContext = createContext();
 
@@ -11,6 +12,37 @@ const ShopContextProvider = (props) => {
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
 
+  // for products state
+  const [cartItems, setCartItems] = useState({});
+
+ 
+  const addToCart = async (itemId,size) => {
+  let cartData =structuredClone(cartItems);
+  // check if the item already exists in the cart
+  // if it does, increment the quantity
+  if (cartData[itemId]) {
+    if (cartData[itemId][size]) {
+      cartData[itemId][size] += 1;
+    }
+    else {
+      cartData[itemId][size] = 1;
+    } 
+  } 
+  else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+   }
+   setCartItems(cartData);
+  }
+
+  
+  useEffect(()=>{
+    console.log(cartItems);
+  }, [cartItems])
+
+
+
+
   const value = {
     products,
     currency,
@@ -19,10 +51,18 @@ const ShopContextProvider = (props) => {
     setSearch,
     showSearch,
     setShowSearch,
+    cartItems,
+    addToCart,
   };
+  
+  ShopContextProvider.propTypes = {
+  children: PropTypes.node.isRequired, // Validates that 'children' is a React node and required
+};
+
 
   return (
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
   );
+  
 };
 export default ShopContextProvider;
