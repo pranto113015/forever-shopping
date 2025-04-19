4a1969292bbfd589fa04d3abb473e344d325ae00
pranto113015 <pranto3876@gmail.com>
2025-04-19 12:28:42 +0600

    add navigation to orders page on Place Order button click

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { assets } from '../assets/assets';
 import Title from '../components/Title';
 import CartTotal from './../components/CartTotal';
+import { ShopContext } from '../context/ShopContext';
 
 const PlaceOrder = () => {
 const [method, setMethod] = useState('cod');
+
+const { navigate } = useContext(ShopContext);
+
 
   return (
     <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
@@ -71,7 +75,7 @@ const [method, setMethod] = useState('cod');
 
 
               <div className='w-full text-end mt-8'>
-                <button className='bg-black text-white px-16 py-3 text-sm'>PLACE ORDER</button>
+                <button onClick={()=>navigate('/orders')} className='bg-black text-white px-16 py-3 text-sm'>PLACE ORDER</button>
               </div>
 
 
