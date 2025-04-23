0cf8c0f51eeeee45cef5ab296806be429887818a
pranto113015 <pranto3876@gmail.com>
2025-04-23 19:39:56 +0600

    add preventDefault method for login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
 const Login = () => {
   const [currentState, setCurrentState] = useState('Login');
+
+
+  const onSubmitHandler = async (event) =>{
+    event.preventDefault();
+  }
+
   return (
     <div>
-      <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+      <form onSubmit={onSubmitHandler} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
           
           <div className="inline-flex items-center gap-2 mb-2 mt-10">
             <p className="prata-regular text-3xl">{currentState}</p>
