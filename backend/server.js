802ebbda104a682560d25af5e723a5b79d382951
pranto113015 <pranto3876@gmail.com>
2025-04-24 22:45:36 +0600

    add mongodb connection code

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
  import express from 'express';
  import cors from 'cors';
  import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+
 
 //  app congig 
     const app = express();
     const port = process.env.PORT || 4000;
+    connectDB();
 
 // middlewares
 app.use(express.json());
