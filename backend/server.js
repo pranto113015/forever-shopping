9541adcbd27fe91df62c7dddbd4af47657c8fe7f
pranto113015 <pranto3876@gmail.com>
2025-04-28 23:58:29 +0600

    Implement product management features with add, list, remove, and single product endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
- import express from 'express';
- import cors from 'cors';
- import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
 
 
 //  app congig 
-    const app = express();
-    const port = process.env.PORT || 4000;
-    connectDB();
-    connectCloudinary
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary
 
 
 // middlewares
@@ -19,6 +20,7 @@ app.use(cors());
 
 //  api endpoints
 app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
 
 
 app.get('/', (req, res) => {
