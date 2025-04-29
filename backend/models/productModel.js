1bc6be3a6a7f791c1de4dcebd0ff6ac49946d270
pranto113015 <pranto3876@gmail.com>
2025-04-30 00:00:29 +0600

    Implement product addition with Cloudinary image upload and fix bestseller field type

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -9,7 +9,7 @@ const productSchema = new mongoose.Schema({
     category:{ type: String, required: true },
     subCategory:{ type: String, required: true},
     sizes:{ type: Array, required: true },
-    bestseller:{ type: Booleanz },
+    bestseller:{ type: Boolean },
     date:{ type: Number, required: true }
 })
 
