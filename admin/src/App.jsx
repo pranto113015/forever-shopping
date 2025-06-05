ed290df4375a9d2ce9426c656f56327ae1cd26e2
pranto113015 <pranto3876@gmail.com>
2025-06-05 20:51:22 +0600

    feat: implement Sidebar component with navigation links; update App component to include Sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
 
 const App = () => {
   return (
-    <>
-    <Navbar />
-    </>
+    <div className='bg-gray-50 min-h-screen'>
+
+      <>
+       <Navbar />
+       
+       <hr />
+
+       <div className='flex w-full'>  
+         <Sidebar />
+       </div>
+       
+      </>
+
+    </div>
   )
 }
 
