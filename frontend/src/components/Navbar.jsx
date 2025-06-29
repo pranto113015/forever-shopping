307b59158f34fbe70048fd9cce686ae018240f72
pranto113015 <pranto3876@gmail.com>
2025-06-29 22:37:42 +0600

    feat: implement logout functionality in Navbar component; navigate to login and clear token and cart items on logout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,17 @@ import { ShopContext } from "../context/ShopContext";
 function Navbar() {
   const [visible, setVisible] = useState(false);
 
-  const { setShowSearch, getCartCount } = useContext(ShopContext);
+  const { setShowSearch, getCartCount, navigate, token, setToken, setCartItems } = useContext(ShopContext);
+
+
+  const logout = () =>{
+    navigate('/login');
+    localStorage.removeItem('token');
+    setToken('');
+    setCartItems({});
+  
+  }
+
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
@@ -49,7 +59,7 @@ function Navbar() {
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My Profie</p>
               <p className="cursor-pointer hover:text-black">Order</p>
-              <p className="cursor-pointer hover:text-black">Logout</p>
+              <p onClick={logout} className="cursor-pointer hover:text-black">Logout</p>
             </div>
           </div>
         </div>
