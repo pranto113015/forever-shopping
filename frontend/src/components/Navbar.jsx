cc7bdeb7082af68a165e33b5b8c1f238040fd70a
pranto113015 <pranto3876@gmail.com>
2025-06-29 23:17:56 +0600

    feat: enhance Navbar component; improve logout functionality and update profile dropdown visibility based on authentication state

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,17 +6,21 @@ import { ShopContext } from "../context/ShopContext";
 function Navbar() {
   const [visible, setVisible] = useState(false);
 
-  const { setShowSearch, getCartCount, navigate, token, setToken, setCartItems } = useContext(ShopContext);
+  const {
+    setShowSearch,
+    getCartCount,
+    navigate,
+    token,
+    setToken,
+    setCartItems,
+  } = useContext(ShopContext);
 
-
-  const logout = () =>{
-    navigate('/login');
-    localStorage.removeItem('token');
-    setToken('');
+  const logout = () => {
+    navigate("/login");
+    localStorage.removeItem("token");
+    setToken("");
     setCartItems({});
-  
-  }
-
+  };
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
@@ -52,22 +56,34 @@ function Navbar() {
         />
 
         <div className="group relative">
-        
-          <Link to={'/login'}><img src={assets.profile_icon} className="w-5 cursor-pointer" alt="pofileIcon"/></Link>
+          <img
+            onClick={() => (token ? null : navigate("/login"))}
+            src={assets.profile_icon}
+            className="w-5 cursor-pointer"
+            alt="pofileIcon"
+          />
 
-          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
-            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
-              <p className="cursor-pointer hover:text-black">My Profie</p>
-              <p className="cursor-pointer hover:text-black">Order</p>
-              <p onClick={logout} className="cursor-pointer hover:text-black">Logout</p>
+          {/* dropdown menu for profile icon */}
+          {token && (
+            <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+              <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+                <p className="cursor-pointer hover:text-black">My Profile</p>
+                <p className="cursor-pointer hover:text-black">Order</p>
+                <p onClick={logout} className="cursor-pointer hover:text-black">
+                  Logout
+                </p>
+              </div>
             </div>
-          </div>
+          )}
+
         </div>
+
+
 
         {/* card icon start */}
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="cartIco" />
-          <p  className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
             {getCartCount()}
           </p>
         </Link>
