f158b98ad614e8a60432a02ca78188004cfa51f2
pranto113015 <pranto3876@gmail.com>
2025-06-30 17:36:37 +0600

    fix: correct cart data handling in addToCart function

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,8 +1,36 @@
 
 // add products to user cart
 const addToCart = async (req, res) => {
+    try {
+        const { userId, itemId, size } = req.body;
 
+        const userData = await uderModel.findById(userId);
+        let cartData = await userData.cartData;
+
+        if (!cartData) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            }
+            else {
+                cartData[itemId][size] = 1;
+            }
+        } else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+
+        await userModel.findByIdAndUpdate(userId, { cartData: cartData });
+        res.json({ success: true, message: "Item added to cart successfully" });
+    
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
 }
+
+
+
+
 
 
 
