f31527c39a1eb491cdfa87e3a7f4c211c6736d33
pranto113015 <pranto3876@gmail.com>
2025-06-30 17:15:15 +0600

    feat: implement cart functionality with routes and authentication middleware

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,26 @@
+
+// add products to user cart
+const addToCart = async (req, res) => {
+
+}
+
+
+
+
+
+// update user cart
+const updateCart = async (req, res) => {
+
+}
+
+
+
+
+// get user cart
+const getUserCart = async (req, res) => {
+
+}
+
+
+
+export { addToCart, updateCart, getUserCart };
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
 import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
+
 
 
 //  app congig 
@@ -18,9 +20,14 @@ connectCloudinary();
 app.use(express.json());
 app.use(cors());
 
+
+
 //  api endpoints
 app.use('/api/user', userRouter);
 app.use('/api/product', productRouter);
+app.use('/api/cart', cartRouter);
+
+
 
 
 app.get('/', (req, res) => {
