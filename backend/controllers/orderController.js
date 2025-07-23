cd912d449ef7ac82c700414b6e5d27155f3b65c5
pranto113015 <pranto3876@gmail.com>
2025-07-23 21:21:16 +0600

    feat: implement orderController with methods for various payment processing and order management

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,43 @@
+// placing order using COD Method
+const placeOrder = async (req, res) => {
+
+}
+
+
+// placing orders using Stripe Method
+const placeOrderStripe = async (req, res) => {
+
+}
+
+
+
+// placing orders using Razorpay Method
+const placeOrderRazorpay = async (req, res) => {
+
+}
+
+
+// all orders data for admin panel
+const allOrders = async (req, res) => {
+
+}
+
+
+
+// user order data for frontend
+const userOrders = async (req, res) => {
+
+}
+
+
+
+
+// update order status from admin panel
+const updateStatus = async (req, res) => {
+
+}
+
+
+export{placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus};
+
+
