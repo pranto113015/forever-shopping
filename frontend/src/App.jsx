9f455f690adaea3d2686f717e0e99ef9c81f5dea
pranto113015 <pranto3876@gmail.com>
2025-07-28 16:00:38 +0600

    add verify page route path in app.jsx file and also import this file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import Orders from "./pages/Orders";
 import Navbar from './components/Navbar';
 import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
+import Verify from "./pages/Verify";
 
 // for notification prodtuct added or no to cart library header
 import { ToastContainer } from 'react-toastify';
@@ -33,6 +34,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/place-order" element={<PlaceOrder />} />
         <Route path="/orders" element={<Orders />} />
+        <Route path="/verify" element={<Verify />} />
       </Routes>
       <Footer/>
     </div>
