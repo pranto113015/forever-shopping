0649fce0446dac1134cc4fc715ac03b22ff40146
pranto113015 <pranto3876@gmail.com>
2025-07-28 21:47:10 +0600

    refactor: replace bcrypt with bcryptjs for password hashing and comparison

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import validator from 'validator';
-import bcrypt from 'bcrypt';
+import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
 
@@ -23,7 +23,7 @@ const loginUser = async (req, res) => {
             return res.json({ success: false, message: 'User does not exist' });
         }
 
-        const isMatch = await bcrypt.compare(password, user.password);
+        const isMatch = await bcryptjs.compare(password, user.password);
 
 
         // if password is matched
@@ -68,8 +68,8 @@ const registerUser = async (req, res) => {
         }
 
         // Hashing password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
 
 
         // Creating new user
