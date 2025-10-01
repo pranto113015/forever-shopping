7b62f8689c3365a61e474f84fc36f3eaa04acb0c
pranto113015 <pranto3876@gmail.com>
2025-10-01 15:37:03 +0600

    feat: add react-icons dependency and enhance Login and Navbar components with icons

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -3,6 +3,8 @@ import { backendUrl } from "../App";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { assets } from "../assets/assets";
+import { MdEmail, MdLock } from "react-icons/md";
+import { FiLogIn } from "react-icons/fi";
 
 const Login = ({ setToken }) => {
   const [email, setEmail] = useState("");
@@ -34,14 +36,15 @@ const Login = ({ setToken }) => {
         <img
           src={assets.logo_login}
           alt="Admin Logo"
-          className="mx-auto mb-1 w-50 h-15 object-contain"
+          className="mx-auto mb-1 w-50 h-15 object-contain "
         />
 
         <h1 className="text-2xl font-bold mb-4 text-center">Admin Panel</h1>
         <form onSubmit={onSubmitHandler}>
+          {/* Email Field */}
           <div className="mb-3 min-w-72">
-            <p className="text-sm font-medium text-gray-700 mb-2">
-              Email Address
+            <p className="flex items-center text-sm font-medium text-gray-700 mb-2">
+              <MdEmail className="mr-2 text-gray-600 text-lg" /> Email Address
             </p>
             <input
               onChange={(e) => setEmail(e.target.value)}
@@ -53,8 +56,11 @@ const Login = ({ setToken }) => {
             />
           </div>
 
+          {/* Password Field */}
           <div className="mb-3 min-w-72">
-            <p className="text-sm font-medium text-gray-700 mb-2">Password</p>
+            <p className="flex items-center text-sm font-medium text-gray-700 mb-2">
+              <MdLock className="mr-2 text-gray-600 text-lg" /> Password
+            </p>
             <input
               onChange={(e) => setPassword(e.target.value)}
               value={password}
@@ -64,11 +70,13 @@ const Login = ({ setToken }) => {
               className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
             />
           </div>
+
+          {/* Login Button */}
           <button
             type="submit"
-            className="mt-2 w-full py-2 px-4 rounded-md text-white bg-black"
+            className="flex items-center justify-center gap-2 mt-2 w-full py-2 px-4 rounded-md text-white bg-black cursor-pointer"
           >
-            Login
+            <FiLogIn className="text-lg" /> Login
           </button>
         </form>
       </div>
